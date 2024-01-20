b4b5c0e3f9d2913f77cce2ea426d0ebcb81d6740
PoyBoy <parvsharma0104@gmail.com>
2024-01-21 04:07:49 +0530

    Added usage of function

--- master_entry.py
+++ master_entry.py
@@ -1,5 +1,6 @@
 from image_2_RGB import colorize
 
-# colorize = file_loc(str), is_img(bool), method(0/1), show_graph(bool), save_image(bool), use_gpu(bool)
+# colorize = file_loc(str), |-> default values|, is_img(bool), method(0/1), show_graph(bool), save_image(bool), use_gpu(bool)
 
-colorize()
+colorize(r"C:\Users\Xx_Poy517us_xX\Pictures\Screenshots\Screenshot (246).png",
+         True, 1, True, True, False)
