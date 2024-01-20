3fd9b5146dbb28d8937acb0d2e99b9de7bb5b6e2
PoyBoy <parvsharma0104@gmail.com>
2024-01-20 15:44:01 +0530

    added notes

--- colorizers/base_color.py
+++ colorizers/base_color.py
@@ -2,23 +2,27 @@
 import torch
 from torch import nn
 
+
 class BaseColor(nn.Module):
-	def __init__(self):
-		super(BaseColor, self).__init__()
+    def __init__(self):
+        super(BaseColor, self).__init__()
 
-		self.l_cent = 50.
-		self.l_norm = 100.
-		self.ab_norm = 110.
+        self.l_cent = 50.
+        self.l_norm = 100.
+        self.ab_norm = 110.
 
-	def normalize_l(self, in_l):
-		return (in_l-self.l_cent)/self.l_norm
+    # These functions are based off of calculations that help to normalize values, make them tend closer to 0,
+        # AB can take negative values, hence the harder division, unnormalizing helps bring it back to the scale
+        # that were originally in
 
-	def unnormalize_l(self, in_l):
-		return in_l*self.l_norm + self.l_cent
+    def normalize_l(self, in_l):
+        return (in_l-self.l_cent)/self.l_norm
 
-	def normalize_ab(self, in_ab):
-		return in_ab/self.ab_norm
+    def unnormalize_l(self, in_l):
+        return in_l*self.l_norm + self.l_cent
 
-	def unnormalize_ab(self, in_ab):
-		return in_ab*self.ab_norm
+    def normalize_ab(self, in_ab):
+        return in_ab/self.ab_norm
 
+    def unnormalize_ab(self, in_ab):
+        return in_ab*self.ab_norm
